5fc20917e77d70e13dfef55d637c43b3a877aca9
Guillaume WOODWARD <gwoodwar@e3r3p7.42.fr>
2016-06-27 18:51:06 +0200

    more work

--- Grace.c
+++ Grace.c
@@ -3,6 +3,6 @@
 	comment
 */
 #define HEAD(fd) char*h="#include<stdio.h>%c/*%c%ccomment%c*/%c#define HEAD(fd) char*h=%c%s%c;dprintf(fd,10,10,9,10,10,34,h,34)"dprintf(fd,10,10,9,10,10,34,h,34);
-#define MACR(fd) char*m="#define MACR(fd) char*m=%c%s%c;dprintf(fd,34,m,34);"dprintf(fd,34,m,34);
-#define MAIN() int main(){fd=open('Grace_kid.c');HEAD(fd)MACR(fd)}
+#define MACR(fd) char*m="%c#define MACR(fd) char*m=%c%s%c;dprintf(fd,10,34,m,34);"dprintf(fd,10,34,m,34);
+#define MAIN() int main(){fd=open('Grace_kid.c');HEAD(fd);MACR(fd);}
 MAIN()
